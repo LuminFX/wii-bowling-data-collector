a34b4496f46b05b87d65c2fdbd972ab781ed9f98
LuminMCB <tmschmitz@cox.net>
2023-07-16 15:49:08 -0700

    makefile and setup

---
+++ Makefile
@@ -0,0 +1,6 @@
+run: 
+	python3 wb-data-collector/main.py
+
+install:
+	pip install -r requirements.txt
+
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "Wii-Bowling-Data-Scraper",
+    version = "1.0",
+    packages=find_packages()
+)
